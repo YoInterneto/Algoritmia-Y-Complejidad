ef8e86c7a0f5d97b52ebbb940cd46c467a8616c2
Alberto González Martínez <albergonmar@gmail.com>
2020-06-13 00:01:46 +0200

    klk

--- aaAlberto/T4/Ejercicio3.py
+++ aaAlberto/T4/Ejercicio3.py
@@ -1,4 +1,10 @@
 
+def printBilletes(billetes):
+    index = 1
+    for billete in billetes:
+        print(str(index)+") "+str(billete)+"€ - ",end = '')
+        index += 1
+
 def pagar(billetes,nbilletes,cantidad):
 
     if len(nbilletes) != len(billetes):
@@ -12,17 +18,17 @@ def pagar(billetes,nbilletes,cantidad):
         #que empezar por el final de la lista
         for index in range(len(billetes)-1,-1, -1):
             billeteValor = billetes[index]
-            print(billeteValor)
             while (nbilletes[index] > 0) & (cantidad > 0) & (billeteValor <= cantidad):
                 cantidad -= billeteValor
                 usados.append(billeteValor)
                 nbilletes[index] -= 1
-        print(usados)
 
         if(cantidad > 0):
-            print("No has podido pagar")
+            print("No has podido pagar el precio justo")
         else:
-            print("Si se ha podido pagar")
+            print("Si se ha podido pagar el precio justo con "+ str(len(usados)) +" billetes")
+
+        printBilletes(usados)
 
 billetes = [1,2,5,10,20,50]
 nbilletes = [3,6,4,3,1,1]
